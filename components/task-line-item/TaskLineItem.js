1dce41aeced584f7d78c3e47b953be7a2e9bde01
Lakha Singh Nagi <polizic@yahoo.co.in>
2017-08-14 18:16:47 +0530

    - Rewritten styles for task-line-item to make it more fluid

--- components/task-line-item/TaskLineItem.js
+++ components/task-line-item/TaskLineItem.js
@@ -6,7 +6,7 @@ import React from 'react';
 class TaskLineItem extends React.Component {
   render() {
     return (
-      <div data-completed={this.props.completed} className="flex task-line-item" id={this.props.id}>
+      <div data-completed={this.props.completed} className="task-line-item" id={this.props.id}>
         <input checked={this.props.completed ? true : false} onChange={this.props.updateTaskStatus} type="checkbox" />
         <input className="no-borders" type="text" value={this.props.name} onChange={this.props.updateTask} />
         <a className="icon-close" href="#" onClick={this.props.deleteTask}>x</a>
