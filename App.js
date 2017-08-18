bdc06610d3e3f0051165a107afce90eaa56984ff
Lakha Singh Nagi <polizic@yahoo.co.in>
2017-08-18 18:38:41 +0530

    commented Header as its not needed for now

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     return (
       <Router>
         <div>
-          <Header />
+          {/* <Header /> */}
           <Route exact path="/" component={ToDoList} />
           <Route path="/contact" component={Contact} />
         </div>
